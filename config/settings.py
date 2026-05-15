b409a6f2846057b2c005001f40203bae502691ec
sergrih1980-sys <sergrih1980@gmail.com>
2026-05-15 20:01:55 +0300

    Написание кода

--- config/settings.py
+++ config/settings.py
@@ -116,6 +116,7 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+USE_L18N = True
 
 USE_TZ = True
 
@@ -124,5 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = (BASE_DIR / 'static',)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
