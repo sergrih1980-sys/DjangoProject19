8c87171a7aacc19314749639a63bdb3b4f273f31
sergrih1980-sys <sergrih1980@gmail.com>
2026-05-17 02:28:28 +0300

    Написание кода

--- config/settings.py
+++ config/settings.py
@@ -118,11 +118,12 @@ USE_L18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
-# STATICFILES_DIRS = (BASE_DIR / "static",)
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"
+]
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
