3e3d996b20073e9296abdba46557472c67738bab
sergrih1980-sys <sergrih1980@gmail.com>
2026-06-21 22:34:24 +0300

    Настройка авторизации

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,19 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'sergrihlobysev1980@yandex.ru'
+EMAIL_HOST_PASSWORD = 'ehhiteeuoqnmhxjx'
+
+
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+LOGIN_URL = '/users/login/'
--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,17 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
-import shop_products
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('blog/', include('blog_app.urls')),
+    path('products/', include('shop_products.urls', namespace='shop_products')),
+    path('users/', include('users.urls')),
 
-urlpatterns = [path("admin/", admin.site.urls),
-               path('catalog/', include('catalog.urls')),
-               path('', include('catalog.urls')),
-               path('blog/', include('blog_app.urls')),
-               path('shop_products/', include('shop_products.urls', namespace='shop_products')),
-               path('', include('users.urls')),
-               ]
 
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
+
+]
